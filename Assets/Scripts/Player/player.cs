c210189cad63c9457d2845af156bf4fbc16a63b0
Nue-X <1962785977@qq.com>
2023-10-18 17:11:24 +0800

    v0.11 修复移动时距离异常的bug

--- Assets/Scripts/Player/player.cs
+++ Assets/Scripts/Player/player.cs
@@ -6,59 +6,62 @@ namespace PTShooter.Assets.Scripts.Player
 	{
 		[Export] private float _normalSpeed = 80.0f;
 		[Export] private float _jumpForce = 100.0f;
+		[Export] private bool _canMoveAir = false;
 		private float _lastMoveDir = 0f;
 
 		private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle(); //将Variant转换为float
-
-		public override void _Ready()
-		{
-
-		}
 
 		public override void _PhysicsProcess(double delta)
 		{
 			//重力
 			if (!IsOnFloor())
+			{
 				Velocity += GetGravityVector((float)delta);
+
+				//处理空中水平移动
+				if (_canMoveAir &&
+					(Input.IsActionPressed("move_left") || Input.IsActionPressed("move_right")))
+					Velocity = GetHorizontalMoveVector(Velocity);
+			}
 			else
-				Velocity = GetMoveVector(Velocity);
+				Velocity = GetHorizontalMoveVector(Velocity);
 			
 			//跳跃
 			if (IsOnFloor() && Input.IsActionPressed("move_jump"))
-				Velocity += GetJumpVector(Velocity);
+				Velocity += GetJumpVector();
 			
 			MoveAndSlide();
 		}
 
 		/// <summary>
-		/// 获取某一时刻玩家的移动向量
+		/// 获取某一时刻玩家的水平移动向量
 		/// </summary>
 		/// <param name="velocity">原节点移动向量</param>
 		/// <returns></returns>
-		private Vector2 GetMoveVector(Vector2 velocity)
+		private Vector2 GetHorizontalMoveVector(Vector2 velocity)
 		{
-			Vector2 targetMoveDir = Vector2.Zero;
+			Vector2 targetHorizontalMovement = Vector2.Zero;
 			float negativeXDir = Input.GetActionStrength("move_left");
 			float positiveXDir = Input.GetActionStrength("move_right");
 
 			//移动指令后覆盖//
 			if ((negativeXDir == 0 && positiveXDir == 0))
 			{
-				targetMoveDir = Vector2.Zero;
+				targetHorizontalMovement = Vector2.Zero;
 				_lastMoveDir = 0f;
 			}
 			else
 			{
 				if (negativeXDir == 0 || positiveXDir == 0)
 				{
-					targetMoveDir = new Vector2(negativeXDir == 0 ? positiveXDir : -negativeXDir, velocity.Y);
+					targetHorizontalMovement = new Vector2((negativeXDir == 0 ? positiveXDir : -negativeXDir) * _normalSpeed, velocity.Y);
 					_lastMoveDir = negativeXDir == 0 ? positiveXDir : -negativeXDir;
 				}
 				else
-					targetMoveDir = new Vector2(-_lastMoveDir, velocity.Y);
+					targetHorizontalMovement = new Vector2(-_lastMoveDir * _normalSpeed, velocity.Y);
 			}
 
-			return targetMoveDir * _normalSpeed;
+			return targetHorizontalMovement;
 		}
 
 		/// <summary>
@@ -75,7 +78,7 @@ namespace PTShooter.Assets.Scripts.Player
 		/// </summary>
 		/// <param name="velocity">节点原移动向量</param>
 		/// <returns></returns>
-		private Vector2 GetJumpVector(Vector2 velocity)
+		private Vector2 GetJumpVector()
 		{
 			return new Vector2(_lastMoveDir, - _jumpForce);
 		}
