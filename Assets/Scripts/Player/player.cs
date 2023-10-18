ac65bed5f28d90220a27f2edf9e15de858c09ed9
Nue-X <1962785977@qq.com>
2023-10-18 16:21:54 +0800

    v0.1 增加简单的角色控制器

--- Assets/Scripts/Player/player.cs
+++ Assets/Scripts/Player/player.cs
@@ -1,52 +1,83 @@
 using Godot;
-using System;
 
-public partial class player : CharacterBody2D
+namespace PTShooter.Assets.Scripts.Player
 {
-   [Export] private float _normalSpeed = 80;
-   [Export] private float _jumpForce = 100;
-   private Vector2 _moveDir = Vector2.Zero;
+	public partial class Player : CharacterBody2D
+	{
+		[Export] private float _normalSpeed = 80.0f;
+		[Export] private float _jumpForce = 100.0f;
+		private float _lastMoveDir = 0f;
 
-   private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle(); //将Variant转换为float
-   public override void _Ready()
-   {
-	  
-   }
+		private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle(); //将Variant转换为float
 
-   public override void _PhysicsProcess(double delta)
-   {
-	   Vector2 temp = Vector2.Zero;
-	   
-	   //受重力影响下落
-	   if (!IsOnFloor())
-		   temp = GetGravityVector(Velocity, (float)delta);
+		public override void _Ready()
+		{
 
-	   Velocity = GetMoveVector(Velocity) + temp;
-	   
-	   MoveAndSlide();
-   }
+		}
 
-   /// <summary>
-   /// 获取玩家移动向量
-   /// </summary>
-   /// <param name="velocity">原节点移动向量</param>
-   /// <returns></returns>
-   private Vector2 GetMoveVector(Vector2 velocity)
-   {
-	   _moveDir = Input.GetVector( "move_left", "move_right",
-		   "move_up", "move_down").Normalized();
-	   return _moveDir * _normalSpeed;
-   }
+		public override void _PhysicsProcess(double delta)
+		{
+			//重力
+			if (!IsOnFloor())
+				Velocity += GetGravityVector((float)delta);
+			else
+				Velocity = GetMoveVector(Velocity);
+			
+			//跳跃
+			if (IsOnFloor() && Input.IsActionPressed("move_jump"))
+				Velocity += GetJumpVector(Velocity);
+			
+			MoveAndSlide();
+		}
 
-   /// <summary>
-   /// 获取某一时刻的重力向量
-   /// </summary>
-   /// <param name="velocity">节点原向量</param>
-   /// <param name="delta">变化率</param>
-   /// <returns></returns>
-   private Vector2 GetGravityVector(Vector2 velocity, float delta)
-   {
-	   return new Vector2(velocity.X, velocity.Y += _gravity * delta);
-   }
-   
+		/// <summary>
+		/// 获取某一时刻玩家的移动向量
+		/// </summary>
+		/// <param name="velocity">原节点移动向量</param>
+		/// <returns></returns>
+		private Vector2 GetMoveVector(Vector2 velocity)
+		{
+			Vector2 targetMoveDir = Vector2.Zero;
+			float negativeXDir = Input.GetActionStrength("move_left");
+			float positiveXDir = Input.GetActionStrength("move_right");
+
+			//移动指令后覆盖//
+			if ((negativeXDir == 0 && positiveXDir == 0))
+			{
+				targetMoveDir = Vector2.Zero;
+				_lastMoveDir = 0f;
+			}
+			else
+			{
+				if (negativeXDir == 0 || positiveXDir == 0)
+				{
+					targetMoveDir = new Vector2(negativeXDir == 0 ? positiveXDir : -negativeXDir, velocity.Y);
+					_lastMoveDir = negativeXDir == 0 ? positiveXDir : -negativeXDir;
+				}
+				else
+					targetMoveDir = new Vector2(-_lastMoveDir, velocity.Y);
+			}
+
+			return targetMoveDir * _normalSpeed;
+		}
+
+		/// <summary>
+		/// 获取某一时刻的重力向量
+		/// </summary>
+		/// <returns></returns>
+		private Vector2 GetGravityVector(float delta)
+		{
+			return new Vector2(0, _gravity * delta);
+		}
+
+		/// <summary>
+		/// 获取某一时刻的跳跃力向量
+		/// </summary>
+		/// <param name="velocity">节点原移动向量</param>
+		/// <returns></returns>
+		private Vector2 GetJumpVector(Vector2 velocity)
+		{
+			return new Vector2(_lastMoveDir, - _jumpForce);
+		}
+	}
 }
